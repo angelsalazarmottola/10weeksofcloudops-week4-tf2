e24c641d1cce8ece859c8be39586d256cabb7c53
angelsalazarmottola <angelsalazarmottola@gmail.com>
2024-12-30 13:30:33 -0300

    first upload

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "10weeksofcloudops-week4-angel"
     key    = "backend/ToDo-App.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
     dynamodb_table = "10weeksofcloudops-dynamo"
   }
 }
